5d0839af463c2f36b924a5d9013cfa98f0d36579
fahadmohaideen <150104527+fahadmohaideen@users.noreply.github.com>
2025-09-01 12:34:18 +0800

    Level-3

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task{
+    protected boolean marked;
+    protected String task;
+
+    public Task(boolean marked_as_done, String task_name){
+        marked = marked_as_done;
+        task = task_name;
+    }
+
+    public void mark(){
+        marked = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("   [X] " + task);
+    }
+
+    public void unmark(){
+        marked = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("   [ ] " + task);
+    }
+}
--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -22,7 +22,8 @@ public class Tom {
         System.out.println("____________________________________");
         greeting();
         System.out.println("____________________________________");
-        String[] list = new String[100];
+        Task[] list = new Task[100];
+        boolean[] marked = new boolean[100];
         Scanner input = new Scanner(System.in);
         int ptr = 0;
         while(true) {
@@ -31,12 +32,29 @@ public class Tom {
                 System.out.println("____________________________________");
                 if (output.equalsIgnoreCase("list")){
                     for(int x=0; x<ptr; x++){
-                        System.out.println(x+1 + ". " + list[x]);
+                        System.out.println("Here are the tasks in your list:");
+                        if(list[x].marked){
+                            System.out.println(x+1 + ". [X] " + list[x].task);
+                        }
+                        else {
+                            System.out.println(x + 1 + ". [ ] " + list[x].task);
+                        }
                     }
                 }
+                else if (output.split(" ")[0].equalsIgnoreCase("mark")){
+                    int index = Integer.parseInt(output.split(" ")[1]) - 1;
+                    list[index].mark();
+                }
+
+                else if (output.split(" ")[0].equalsIgnoreCase("unmark")){
+                    int index = Integer.parseInt(output.split(" ")[1]) - 1;
+                    list[index].unmark();
+                }
+
                 else{
                     System.out.println("added:" + output);
-                    list[ptr] = output;
+                    Task new_task = new Task(false, output);
+                    list[ptr] = new_task;
                     ptr++;
                 }
                 System.out.println("____________________________________");
