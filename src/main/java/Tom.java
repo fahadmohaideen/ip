92a08cf360a4514552744a04a427fd85e3bc212d
fahadmohaideen <150104527+fahadmohaideen@users.noreply.github.com>
2025-09-03 07:44:36 +0800

    A-CodeQuality

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -85,40 +85,6 @@ public class Tom {
                     break;
 
             }
-            /*if(!output.equalsIgnoreCase("Bye")){
-                System.out.println("____________________________________");
-                if (output.equalsIgnoreCase("list")){
-                    for(int x=0; x<ptr; x++){
-                        System.out.println("Here are the tasks in your list:");
-                        if(list[x].marked){
-                            System.out.println(x+1 + ". [X] " + list[x].task);
-                        }
-                        else {
-                            System.out.println(x + 1 + ". [ ] " + list[x].task);
-                        }
-                    }
-                }
-                else if (output.split(" ")[0].equalsIgnoreCase("mark")){
-                    int index = Integer.parseInt(output.split(" ")[1]) - 1;
-                    list[index].mark();
-                }
-
-                else if (output.split(" ")[0].equalsIgnoreCase("unmark")){
-                    int index = Integer.parseInt(output.split(" ")[1]) - 1;
-                    list[index].unmark();
-                }
-
-                else{
-                    System.out.println("added:" + output);
-                    Task new_task = new Task(false, output);
-                    list[ptr] = new_task;
-                    ptr++;
-                }
-                System.out.println("____________________________________");
-            }
-            else{
-                break;
-            }*/
         }
     }
 }
