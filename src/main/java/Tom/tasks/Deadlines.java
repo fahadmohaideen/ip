648fb882fe7ed4c726fa5caa261c23f3031ed90e
fahadmohaideen <150104527+fahadmohaideen@users.noreply.github.com>
2025-09-30 15:23:57 +0800

    branch-Level-6 (Delete)

--- src/main/java/Tom/tasks/Deadlines.java
+++ src/main/java/Tom/tasks/Deadlines.java
@@ -2,16 +2,16 @@ package Tom.tasks;
 
 public class Deadlines extends Task {
 
-    public Deadlines(boolean marked_as_done, String event){
-        super(marked_as_done, event);
+    public Deadlines(boolean marked_as_done, String event, String description){
+        super(marked_as_done, event, description);
     }
 
     public void print(){
         if(this.marked){
-            System.out.println(". [D][X] " + this.task);
+            System.out.println(". [D][X] " + this.task_description);
         }
         else {
-            System.out.println(". [D][] " + this.task);
+            System.out.println(". [D][] " + this.task_description);
         }
     }
 }
